5984569ad233a9ed9b1c316e7e84701e313474fb
Simone van Gompel <simonevangompel@hotmail.com>
2013-12-10 16:26:43 +0100

    action bar is implemented

--- Jokes-app/src/com/example/jokes/MainActivity.java
+++ Jokes-app/src/com/example/jokes/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.jokes;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
+
+	// favorites is nothing more than a special jokes_list
+	public static final int FAVORITES  = 0;
+	public static final int JOKES_LIST = 0;
+	public static final int JOKE	   = 1;
+	public static final int PROFILE    = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // set jokes list to most recent / most popular
     }
 
 
@@ -20,4 +31,27 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void showJoke(View view) {
+    	ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper);
+    	vf.setDisplayedChild(JOKE);
+    	// get joke and show
+    }
+    
+    public void showSearchedJokes(View view) {
+    	ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper);
+    	vf.setDisplayedChild(JOKES_LIST);
+    	// set jokes list
+    }
+    
+    public void showProfile(View view) {
+    	ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper);
+    	vf.setDisplayedChild(PROFILE);
+    	// get profile and show
+    }
+    
+    public void showFavorites(View view) {
+    	ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper);
+    	vf.setDisplayedChild(FAVORITES);
+    	// set jokes list
+    }
 }
