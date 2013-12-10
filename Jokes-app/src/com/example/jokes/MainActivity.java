003ef9245115bca23db699266dbe5d9ff336725a
Simone van Gompel <simonevangompel@hotmail.com>
2013-12-10 19:07:28 +0100

    made list clickable

--- Jokes-app/src/com/example/jokes/MainActivity.java
+++ Jokes-app/src/com/example/jokes/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.jokes;
 
-import android.app.ActionBar;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
@@ -21,8 +27,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         // set jokes list to most recent / most popular
+        ArrayList<String> jokes = new ArrayList<String>();
+        jokes.add("#1");
+        jokes.add("#2");
+        jokes.add("#3");
+        jokes.add("#4");
+        jokes.add("#5");
+        jokes.add("#6");
+        jokes.add("#7");
+        jokes.add("#8");
+        setJokes(jokes);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,10 +46,51 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void showJoke(View view) {
+    public void setJokes(ArrayList<String> jokes){
+    	final ArrayList<String> list = jokes;
+    	ListView listView = (ListView) findViewById(R.id.jokes_list);
+    	final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+    	listView.setAdapter(adapter);
+    	
+    	listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    		@Override
+    		public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+    			final String item = (String) parent.getItemAtPosition(position);
+    			// pak het Joke element en geef deze mee aan showJoke
+    			showJoke(item);
+    			Log.v("TEST",item);
+    		}
+    	});
+    	
+    	/*
+    	Best wel grappig, verwijder een item uit de list met een animatie
+    	
+    	listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    		@Override
+    		public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+    			final String item = (String) parent.getItemAtPosition(position);
+    			view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
+    				@Override
+    				public void run() {
+    					list.remove(item);
+    					adapter.notifyDataSetChanged();
+    					view.setAlpha(1);
+    				}
+    			});
+    		}
+    	});*/
+    	
+    }
+    
+    // voor nu wordt er een string meegegeven, dit moet uiteindelijk een Joke element worden
+    public void showJoke(String joke){
+		Log.v("TEST",joke);
     	ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper);
     	vf.setDisplayedChild(JOKE);
-    	// get joke and show
+    	TextView tv = (TextView) findViewById(R.id.joke_name);
+    	tv.setText("TESTING TESTER TEST!");
+    	TextView tv2 = (TextView) findViewById(R.id.joke_author);
+    	tv2.setText("TESTING TESTER TEST!");
     }
     
     public void showSearchedJokes(View view) {
