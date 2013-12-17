cdb11299ca5f77e7593a91fd8cc807f9df7ff568
Tim <Bloeme>
2013-12-17 23:56:03 +0100

    de nieuwe php's rest werkt nog niet

--- Jokes-app/src/com/example/jokes/Rest.java
+++ Jokes-app/src/com/example/jokes/Rest.java
@@ -1,11 +1,7 @@
 package com.example.jokes;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
@@ -13,11 +9,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
  
 public class Rest extends AsyncTask<String, Void, Void> {
 	
@@ -49,9 +43,6 @@ public class Rest extends AsyncTask<String, Void, Void> {
         	to_send[1] = data;
         	
         	doInBackground(to_send);
-        	
-        	String OutputData = "";
-            JSONObject jsonResponse;
         	
         	return 1;
         }
@@ -95,20 +86,23 @@ public class Rest extends AsyncTask<String, Void, Void> {
         
         public int get_joke(String url, String id){
         	String[] to_send = new String[2]; 
-            
+            Log.v("get_joke", "hi");
             String OutputData = "";
             JSONObject jsonResponse;
+            Log.v("get_joke", "hi");
             
             try{
             	data ="&" + URLEncoder.encode("id", "UTF-8") + "=" + id;
     		}catch(Exception e){
     			Log.v("error",String.valueOf(e));
     		}
+            Log.v("get_joke", "hi");
         	
         	to_send[0] = url;
         	to_send[1] = data;
         	
         	doInBackground(to_send);
+            Log.v("get_joke", "hi");
         	
         	try {
                 
@@ -118,21 +112,24 @@ public class Rest extends AsyncTask<String, Void, Void> {
                 /***** Returns the value mapped by name if it exists and is a JSONArray. ***/
                 /*******  Returns null otherwise.  *******/
                 JSONArray jsonMainNode = jsonResponse.optJSONArray("Android");
+                Log.v("get_joke", "hi");
                   
                 /*********** Process each JSON Node ************/
 
                 int lengthJsonArr = jsonMainNode.length();  
 
                 for(int i=0; i < lengthJsonArr; i++){
+                    Log.v("get_joke", "hi");
                     /****** Get Object for each JSON node.***********/
                     JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
                       
                     /******* Fetch node values **********/
-                    String J_title       = jsonChildNode.optString("title").toString();
-                    String J_joke     = jsonChildNode.optString("joke").toString();
+                    String J_title= jsonChildNode.optString("title").toString();
+                    String J_joke = jsonChildNode.optString("joke").toString();
                     String J_user = jsonChildNode.optString("user").toString();
                       
-                    
+
+                    Log.v("get_joke", "hi");
                     OutputData += "title           : "+ J_title + "Joke      : "+ J_joke + "User                : "+ J_user;
                     
                      
@@ -151,58 +148,18 @@ public class Rest extends AsyncTask<String, Void, Void> {
         
         // Call after onPreExecute method
         protected Void doInBackground(String... in_data) {
-             
-            /************ Make Post Call To Web Server ***********/
-            BufferedReader reader=null;
-    
-                 // Send data 
-                try
-                { 
-                   
-                   // Defined URL  where to send data
-                  URL url = new URL(in_data[0]);
-                  data = in_data[1];
-                   
-                  // Send POST data request
-        
-                  URLConnection conn = url.openConnection(); 
-                  conn.setDoOutput(true); 
-                  OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream()); 
-                  wr.write( data ); 
-                  wr.flush(); 
-               
-                  // Get the server response 
-                    
-                  reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                  StringBuilder sb = new StringBuilder();
-                  String line = null;
-                 
-                    // Read Server Response
-                    while((line = reader.readLine()) != null)
-                        {
-                               // Append server response in string
-                               sb.append(line + "");
-                        }
-                     
-                    // Append Server Response To Content String 
-                   Content = sb.toString();
+        	AndroidHttpClient httpClient = new AndroidHttpClient("http://www.timbloeme.nl");
+            httpClient.setMaxRetries(5);
+            ParameterMap params = httpClient.newParams()
+                    .add("id", "0");
+            httpClient.post("/app/getjokes.php", params, new AsyncCallback() {
+                public void onSuccess(HttpResponse httpResponse) {
+                    Log.v("get_joke", httpResponse.getBodyAsString());
                 }
-                catch(Exception ex)
-                {
-                    Error = ex.getMessage();
+                public void onError(Exception e) {
+                    e.printStackTrace();
                 }
-                finally
-                {
-                    try
-                    {
-          
-                        reader.close();
-                    }
-        
-                    catch(Exception ex) {}
-                }
-             
-            /*****************************************************/
+            });
             return null;
         }
           
--- getjokes.php
+++ getjokes.php
@@ -1,16 +1,16 @@
 <?php 
     $con=mysqli_connect("db.timbloeme.nl","md137126db280854","iYPIzNwH","md137126db280854");
-    
-    $id = urldecode($_POST['id']);
+    if(isset($_POST['id']))
+        $id = urldecode($_POST['id']);
+    else
+        $id = "0";
     $ids = explode(",",id);
-    $nr_ids = count(ids)
+    $nr_ids = count(ids);
     
     $jsonData      = array();
     $jsonTempData  = array();
     
-    $q = "SELECT * FROM jokes " 
-    
-    if(ids[0]=="0"){
+    if($ids[0]=="0"){
         $result = mysqli_query($con,"SELECT * FROM jokes ORDER BY id DESC LIMIT 10");
         while($row = mysqli_fetch_array($result)){
             $jsonTempData['tittle']= $row[2];
@@ -20,9 +20,9 @@
          
         $jsonData[] = $jsonTempData;    
     }else{
-        for($i=0; $i<nr_ids; $i++){
-            $result = mysqli_query($con,"SELECT * FROM jokes WHERE id ="+ids[0]);
-            $row = mysqli_fetch_array($result)
+        for($i=0; $i<$nr_ids; $i++){
+            $result = mysqli_query($con,"SELECT * FROM jokes WHERE id =" . $ids[0]);
+            $row = mysqli_fetch_array($result);
             
             $jsonTempData['tittle']= $row[2];
             $jsonTempData['joke']  = $row[3];
@@ -37,6 +37,4 @@
       
      // Encode Array To JSON Data
      print_r( json_encode($outputArr));
-      
- 
  ?>
